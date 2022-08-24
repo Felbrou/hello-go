95427e110f2bfb5fea1f9382a8cc1f624d48c43f
Felbrou <15351171+Felbrou@users.noreply.github.com>
2022-08-24 13:55:14 -0300

    getting started

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Projects/hello-go
+
+go 1.19
---
+++ hello.go
@@ -0,0 +1,17 @@
+package main /*Declara um pacote main como principal(Um pacote é uma forma de agrupar funções e é composto
+por todos os arquivos no mesmo diretório*/
+
+/*
+Importando o popular pacomete 'fmt' o qual contém funções para formatar texto incluindo o print no console
+*/
+
+import "fmt"
+
+/*
+Implementando a função main para printar a mensagem no console.Uma função main executa por padrão quando o
+package main é executado
+*/
+
+func main() {
+	fmt.Println("Hello, World!")
+}
